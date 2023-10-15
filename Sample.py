2ea43ed2e27e166a01699343ec17018a128b2625
Dimuthu Ruwantha <gunathilaka.edu@gmail.com>
2023-10-15 13:31:42 +0530

    first commit

---
+++ Sample.py
@@ -0,0 +1,14 @@
+# Input and get your summation  (using 1st Git Commitment with main branch)
+
+#Input values
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+#Summation Processe
+sum = float(num1) + float(num2)
+
+#Out put Processed value
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+
+
